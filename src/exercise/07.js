fbbae8913b60912f93ea6c3706511e630bf03707
Nicolas Bourlès <n.bourles@gmail.com>
2025-11-15 07:43:19 +0100

    feat: Promises et fonctions Asynchrones

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -2,18 +2,50 @@
 // Promise aync await
 // http://localhost:3000/isolated/final/07.js
 
-import displayText, { App, init } from "../lib/exerciseHelper";
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init('Promise aync await')
+  init("Promise aync await")
 
-// 🐶 Créé un Promise `calculNombrePair` qui detecte si la parametre en entrée est un nombre impair
-// utilise modulo pour savoir si un nombre est pair
-// 🤖 if (nombre % 2 == 0)
+  // 🐶 Créé un Promise `calculNombrePair` qui detecte si la parametre en entrée est un nombre impair
+  // utilise modulo pour savoir si un nombre est pair
+  // 🤖 if (nombre % 2 == 0)
+  function calculNombrePair(number = 1) {
+    return new Promise((resolve, reject) => {
+      if (number % 2 === 0) {
+        resolve(`OK`)
+      } else {
+        reject(`KO`)
+      }
+    })
+  }
 
-// 🐶 Appelle cette Promise avec les nombres 2 et 3 et gère les cas ok et ko dans la console
+  // 🐶 Appelle cette Promise avec les nombres 2 et 3 et gère les cas ok et ko dans la console
+  calculNombrePair(2).then(
+    (e) => console.log(e),
+    (err) => console.error(err)
+  )
 
-// 🐶 Appelle cette Promise en asynchrone
-// 🤖 async function
-// 🤖 await calculNombrePair
-};
-export default () => <App exercice={exercice} />;
+  calculNombrePair(3).then(
+    (e) => console.log(e),
+    (err) => console.error(err)
+  )
+
+  // 🐶 Appelle cette Promise en asynchrone
+  // 🤖 async function
+  // 🤖 await calculNombrePair
+  async function fetchAsyncCalculNombrePair(number) {
+    const result = await calculNombrePair(number)
+    return `Nombre pair : ${result}`
+  }
+
+  fetchAsyncCalculNombrePair(2).then(
+    (e) => console.log(e),
+    (err) => console.log(err)
+  )
+
+  fetchAsyncCalculNombrePair(3).then(
+    (e) => console.log(e),
+    (err) => console.log(err)
+  )
+}
+export default () => <App exercice={exercice} />
